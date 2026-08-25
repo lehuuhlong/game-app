f55f9c96cf8e4ad4be060dd43fd56884fc17a41a
lehuuhlong <lehuuhlong@gmail.com>
2026-08-25 18:37:21 +0700

    feat: implement PerformanceProvider to manage adaptive graphics modes and optimize 3D components for low-power or restricted devices

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,5 +1,7 @@
 export { ThemeProvider, useTheme } from "./ThemeProvider";
+export { PerformanceProvider, usePerformance } from "./PerformanceProvider";
 export { Navbar } from "./Navbar";
 export { Footer } from "./Footer";
 export { GameCard } from "./GameCard";
 export { StatCard } from "./StatCard";
+
